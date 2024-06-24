51a5ff437247334a99c68ba47d460431d0d35325
Robert Bauer <rbauer1337@gmail.com>
2024-06-23 21:10:28 -0400

    Initial Commit

---
+++ rsb-mobile-ble/rsb_mobile_bleApp.swift
@@ -0,0 +1,17 @@
+//
+//  rsb_mobile_bleApp.swift
+//  rsb-mobile-ble
+//
+//  Created by Astro on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct rsb_mobile_bleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
